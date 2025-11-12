be73fe4f7109978549c36d9862937273f4a77b0d
AMO129 <moses.abraham@pau.edu.ng>
2025-11-12 16:39:44 +0100

    Invoking Function practice_2

--- Week-7/project-1/target/debug/deps/project_1.d
+++
@@ -1,5 +0,0 @@
-C:\Users\NEW\Documents\m.abrahamCOS101\Week-7\project-1\target\debug\deps\project_1.d: src\main.rs
-
-C:\Users\NEW\Documents\m.abrahamCOS101\Week-7\project-1\target\debug\deps\project_1.exe: src\main.rs
-
-src\main.rs:
--- Week-7/project-1/target/debug/project-1.d
+++
@@ -1,1 +0,0 @@
-C:\Users\NEW\Documents\m.abrahamCOS101\Week-7\project-1\target\debug\project-1.exe: C:\Users\NEW\Documents\m.abrahamCOS101\Week-7\project-1\src\main.rs
