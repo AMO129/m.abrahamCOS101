5d20d06fd0e4ac8dbfad6427e914382d0beb6135
AMO129 <moses.abraham@pau.edu.ng>
2025-11-19 18:00:15 +0100

    Initializing and accessing elements

---
+++ week-8/practice-7/target/debug/deps/practice_7.d
@@ -0,0 +1,5 @@
+C:\Users\NEW\Documents\m.abrahamCOS101\week-8\practice-7\target\debug\deps\practice_7.d: src\main.rs
+
+C:\Users\NEW\Documents\m.abrahamCOS101\week-8\practice-7\target\debug\deps\practice_7.exe: src\main.rs
+
+src\main.rs:
