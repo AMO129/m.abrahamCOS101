a00169bc980fc63079b4fa3a7d2e3f49aee89ca5
AMO129 <moses.abraham@pau.edu.ng>
2025-12-03 17:19:25 +0100

    Rust-Structure

---
+++ Week-10/practice-7/target/debug/deps/practice_7.d
@@ -0,0 +1,5 @@
+C:\Users\NEW\Documents\m.abrahamCOS101\Week-10\practice-7\target\debug\deps\practice_7.d: src\main.rs
+
+C:\Users\NEW\Documents\m.abrahamCOS101\Week-10\practice-7\target\debug\deps\practice_7.exe: src\main.rs
+
+src\main.rs:
