c2389a4c0eb482c4f67f0dad041f690a2a728f79
AMO129 <moses.abraham@pau.edu.ng>
2025-12-03 17:11:02 +0100

    Mutable reference in rust borrowing

---
+++ Week-10/practice-6/target/debug/deps/practice_6.d
@@ -0,0 +1,5 @@
+C:\Users\NEW\Documents\m.abrahamCOS101\Week-10\practice-6\target\debug\deps\practice_6.d: src\main.rs
+
+C:\Users\NEW\Documents\m.abrahamCOS101\Week-10\practice-6\target\debug\deps\practice_6.exe: src\main.rs
+
+src\main.rs:
